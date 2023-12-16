78226ed913cd009724ed0345e85a49b47b34401d
Michaiah Bos <michaiahbos@yahoo.com>
2023-12-16 17:19:46 -0600

    wip

---
+++ src/reducers/dragonListReducer.js
@@ -0,0 +1,26 @@
+export const initialState = {
+    newMember: '',
+    members: [
+      { name: 'Jojo Zhang', dragonStatus: true },
+      { name: 'Brandon Harris', dragonStatus: false }
+    ]
+  };
+
+const dragonListReducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_NEW_MEMBER:
+      return {
+        ...state,
+       newMember: action.payload
+      };
+    case ADD_MEMBER:
+      return {
+        ...state,
+      members: [...state.members, action.payload ]
+      };
+    default:
+      return state;
+  }
+};
+
+export default dragonListReducer;
